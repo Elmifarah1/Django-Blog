df6c1512a06cb2b4fe081f3bac6245bf5fa39e46
Elmi Farah <elmifarah98@gmail.com>
2024-06-14 16:07:40 +0000

    fix code

--- codestar/settings.py
+++ codestar/settings.py
@@ -24,10 +24,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ.get('Do8coNFhRv')
+SECRET_KEY = ('Do8coNFhRv')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = [
     "8000-elmifarah1-djangoblog-22wcpdc4lip.ws-eu114.gitpod.io', '.herokuapp.com"
