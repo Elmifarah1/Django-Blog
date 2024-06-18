fd2edc6f3167e57d9d290134c0d84fe2c28e9f6b
Elmi Farah <elmifarah98@gmail.com>
2024-06-18 12:19:02 +0000

    Created Views homepage to show output to users

--- codestar/settings.py
+++ codestar/settings.py
@@ -27,7 +27,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = ('Do8coNFhRv')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = [
     '8000-elmifarah1-djangoblog-22wcpdc4lip.ws-eu114.gitpod.io',
