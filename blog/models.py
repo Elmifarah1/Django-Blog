6ea743de2ff2f398578ec140301d7500dccba6f7
Elmi Farah <elmifarah98@gmail.com>
2024-06-19 12:57:22 +0000

    adding more posts

--- blog/models.py
+++ blog/models.py
@@ -1,11 +1,10 @@
 from django.db import models
 from django.contrib.auth.models import User
-# Define STATUS choices before using it in the model
-STATUS = (
-    (0, "Draft"),
-    (1, "Published")
-)
-# Create your models here
+
+# Create your models here.
+STATUS = ((0, "Draft"), (1, "Published"))
+
+
 class Post(models.Model):
     title = models.CharField(max_length=200, unique=True)
     slug = models.SlugField(max_length=200, unique=True)
@@ -17,25 +16,25 @@ class Post(models.Model):
     status = models.IntegerField(choices=STATUS, default=0)
     excerpt = models.TextField(blank=True)
     updated_on = models.DateTimeField(auto_now=True)
+
     class Meta:
         ordering = ["-created_on"]
+
     def __str__(self):
-        return f"The title of this post is {self.title}"
+        return f"{self.title} | written by {self.author}"
+
 
 class Comment(models.Model):
-    post = models.ForeignKey(
-        Post, on_delete=models.CASCADE, related_name="comments")
-    author = models.ForeignKey(
-        User, on_delete=models.CASCADE, related_name="commenter")
+    post = models.ForeignKey(Post, on_delete=models.CASCADE,
+                             related_name="comments")
+    name = models.CharField(max_length=80)
+    email = models.EmailField()
     body = models.TextField()
-    approved = models.BooleanField(default=False)
     created_on = models.DateTimeField(auto_now_add=True)
+    approved = models.BooleanField(default=False)
+
     class Meta:
         ordering = ["created_on"]
 
     def __str__(self):
-        return f"Comment {self.body} by {self.author}"
-
-
-
-
+        return f"Comment {self.body} by {self.name}"
